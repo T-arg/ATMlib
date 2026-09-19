0c5e334a8d9547ddfed53a8fc360856b725270ce
JO3RI <joerigantois@gmail.com>
2026-09-19 17:32:44 +0100

    Frequency slide bug
    fixed a frequency slide bug
    added:
    
    if ((cmd - 64) == 5) ch->volFreConfig |= 0x40;

--- src/ATMlib.cpp
+++ src/ATMlib.cpp
@@ -280,6 +280,7 @@ void ATM_playroutine() {
             case 2: case 5: // Slide volume/frequency ON advanced
               ch->volFreSlide = pgm_read_byte(ch->ptr++);
               ch->volFreConfig = pgm_read_byte(ch->ptr++);
+              if ((cmd - 64) == 5) ch->volFreConfig |= 0x40;
               break;
             case 3: case 6: // Slide volume/frequency OFF (same as 0x01 0x00)
               ch->volFreSlide = 0;
